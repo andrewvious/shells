e2b47f96314fabb47b125a0e3c516dbfecb6793e
Andrew O'Brien <ohbandrew@gmail.com>
2024-02-05 14:44:15 -0600

    add bit of docs

--- python3/shell.nix
+++ python3/shell.nix
@@ -1,3 +1,14 @@
+# A developement shell for compiling Python.
+# This file needs to be in the root of the project that uses Python.
+#
+# For installation steps see: https://nix.dev/tutorials/install-nix
+#
+# To start the dev shell, simply run: `nix-shell`
+#
+# By default `nix-shell` opens a new `bash` shell. If you would like to use
+# your own, for example `zsh`, you can open a new instance directly after: `zsh`.
+# For more information on this limitation see https://nixos.wiki/wiki/Development_environment_with_nix-shell#direnv
+
 let
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
